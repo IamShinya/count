0e689ba154c5026d48cd0b4030d4bf7e38f905de
Shinya <zhenyejinwan74@gmail.com>
2023-11-24 22:04:10 +0900

    ・ボタンのテキストを変更　index.html ・カウントが１０を超えたらテキストを変更　script.js

--- dist/scripts/script.js
+++ dist/scripts/script.js
@@ -5,6 +5,11 @@ btn.addEventListener('click',function() {
   const result = document.getElementById('result');
   const pulsNum = 1;
   let numResult = Number(result.textContent) + pulsNum;
-  result.textContent = numResult;
+  if(numResult <= 10){
+    result.textContent = numResult;
+  }else{
+    result.textContent = 'LIMIT!!';
+  }
+  
 
 },false);
