09927ed6f81440df63f4cd0d6a6f63c15b62ffba
Shinya <zhenyejinwan74@gmail.com>
2023-11-25 00:17:06 +0900

    微調整 リミットに達したら、アラートを表示

--- dist/scripts/script.js
+++ dist/scripts/script.js
@@ -1,15 +1,22 @@
 const btn = document.getElementById('btn');
+const result = document.getElementById('result');
+
+
+
+
+
+
+
 
 btn.addEventListener('click',function() {
   
-  const result = document.getElementById('result');
+  // const result = document.getElementById('result');
   const pulsNum = 1;
   let numResult = Number(result.textContent) + pulsNum;
   if(numResult <= 10){
     result.textContent = numResult;
   }else{
     result.textContent = 'LIMIT!!';
+    window.alert('カウントが上限に達しました！');
   }
-  
-
 },false);
