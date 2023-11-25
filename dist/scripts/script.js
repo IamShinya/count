b75dca6c371505ce9517b20af83276a380ac0de5
Shinya <zhenyejinwan74@gmail.com>
2023-11-25 23:10:32 +0900

    ID属性をClassへ変更。 それに伴いJSファイルを変更。 LIMIT！！に達したら文字色を赤に変更。

--- dist/scripts/script.js
+++ dist/scripts/script.js
@@ -1,14 +1,13 @@
-const btn = document.getElementById('btn');
-const result = document.getElementById('result');
+const btn = document.querySelector('.btn');
+const result = document.querySelector('.result');
 
 btn.addEventListener('click',function() {
-  
-  // const result = document.getElementById('result');
   const pulsNum = 1;
   let numResult = Number(result.textContent) + pulsNum;
   if(numResult <= 10){
     result.textContent = numResult;
   }else{
+    result.classList.add('limit');
     result.textContent = 'LIMIT!!';
     window.alert('カウントが上限に達しました！');
   }
