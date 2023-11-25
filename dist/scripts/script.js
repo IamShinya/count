e4e60bde83740175885ee39484d0153fc5243ab3
Shinya <zhenyejinwan74@gmail.com>
2023-11-26 00:28:37 +0900

    カウント上限に達したらダイアログを表示し、選択により分岐させる

--- dist/scripts/script.js
+++ dist/scripts/script.js
@@ -1,15 +1,20 @@
 const btn = document.querySelector('.btn');
 const result = document.querySelector('.result');
 
+//ボタンをクリックする度に数字がカウントアップ
+//上限に達したらテキスト変更か、ページリロード
 btn.addEventListener('click',function() {
   const pulsNum = 1;
   let numResult = Number(result.textContent) + pulsNum;
   if(numResult <= 10){
     result.textContent = numResult;
   }else{
-    result.classList.add('limit');
-    result.textContent = 'LIMIT!!';
-    window.alert('カウントが上限に達しました！');
+    if(window.confirm('カウントが上限に達しました！\n最初からカウントし直しますか？')){
+      location.reload();
+    }else{
+      result.classList.add('limit');
+      result.textContent = 'LIMIT!!';
+    }
   }
   
 },false);
